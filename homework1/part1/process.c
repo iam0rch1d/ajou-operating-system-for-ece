34b511553663e233e937865a29fc188402804949
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-08-01 01:19:00 +0900

    Refactor code, Unify coding style to snake_case

--- homework1/part1/process.c
+++ homework1/part1/process.c
@@ -5,69 +5,69 @@
 #include <linux/sched/task.h>
 #include <linux/sched/signal.h>
 
-void traverse_depthfirst(struct task_struct *task) {
+void traverse_dfs(struct task_struct *task) {
+    struct list_head *child_list;
 	struct task_struct *child;
-	struct list_head *listChildren;
 
 	printk(KERN_INFO "%-20s | %5d %5d\n", task->comm, task->pid, task->parent->pid);
 
-	list_for_each(listChildren, &task->children) {
-		child = list_entry(listChildren, struct task_struct, sibling);
-		traverse_depthfirst(child);
+	list_for_each (child_list, &task->children) {
+		child = list_entry(child_list, struct task_struct, sibling);
+
+		traverse_dfs(child);
 	}
 }
 
-void traverse_breadthfirst(struct task_struct *task) {
+void traverse_bfs(struct task_struct *task) {
+    int is_root_printed = 0;
 	struct task_struct *child;
-	struct list_head *listChildren;
-	int printRoot = 1;
+	struct list_head *child_list;
 
-	for_each_process(task) {
-		if (printRoot == 1) {
-			printk(KERN_INFO "%-20s | %5d %5d\n", task->parent->comm, task->parent->pid, 0);
+	for_each_process (task) {
+		if (is_root_printed == 0) {
+			printk(KERN_INFO " %-20s | %5d %5d\n", task->parent->comm, task->parent->pid, 0);
 			
-			list_for_each(listChildren, &task->parent->children) {
-				child = list_entry(listChildren, struct task_struct, sibling);
+			list_for_each(child_list, &task->parent->children) {
+				child = list_entry(child_list, struct task_struct, sibling);
+
 				printk(KERN_INFO "%-20s | %5d %5d\n", child->comm, child->pid, child->parent->pid);
 			}
 
-			printRoot = 0;
+			is_root_printed = 1;
 		}
 		
-		list_for_each(listChildren, &task->children) {
-			child = list_entry(listChildren, struct task_struct, sibling);
-			printk(KERN_INFO "%-20s | %5d %5d\n", child->comm, child->pid, child->parent->pid);
+		list_for_each(child_list, &task->children) {
+			child = list_entry(child_list, struct task_struct, sibling);
+
+			printk(KERN_INFO " %-20s | %5d %5d\n", child->comm, child->pid, child->parent->pid);
 		}
 	}
 }
 
-int listingtasks_init(void) {
+int print_processes_sortedby_dfs(void) {
 	printk(KERN_INFO "Loading Module\n");
 	printk(KERN_INFO "\n");
-	printk(KERN_INFO "Process sorted by depth-first search\n");
-	printk(KERN_INFO "CMD                  | PID   PPID\n");
-	printk(KERN_INFO "---------------------+------------\n");
-
-	traverse_depthfirst(&init_task);
+	printk(KERN_INFO "Processes sorted by depth-first search\n");
+	printk(KERN_INFO " CMD                  | PID   PPID\n");
+	printk(KERN_INFO "----------------------+------------\n");
+	traverse_dfs(&init_task);
 	printk(KERN_INFO "\n");
 	
 	return 0;
 }
 
-void listingtasks_exit(void) {
-	printk(KERN_INFO "Process sorted by breadth-first search\n");
-	printk(KERN_INFO "CMD                  | PID   PPID\n");
-	printk(KERN_INFO "---------------------+------------\n");
-
-	traverse_breadthfirst(&init_task);
+void print_processes_sortedby_bfs(void) {
+	printk(KERN_INFO "Processes sorted by breadth-first search\n");
+	printk(KERN_INFO " CMD                  | PID   PPID\n");
+	printk(KERN_INFO "----------------------+------------\n");
+	traverse_bfs(&init_task);
 	printk(KERN_INFO "\n");
-
 	printk(KERN_INFO "Removing Module\n");
 }
 
-module_init(listingtasks_init);
-module_exit(listingtasks_exit);
+module_init(print_processes_sortedby_dfs);
+module_exit(print_processes_sortedby_bfs);
 
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("HW1 - Pt.1 Linux kernel module for listing tasks");
+MODULE_DESCRIPTION("Homework 1 - Part 1. Linux Kernel Module for Listing Tasks");
 MODULE_AUTHOR("Junyeong Yoon 201723300");
