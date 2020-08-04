1990d50d18d82aeebd4888ee1670599cc057a139
iam0rch1d <0rch1d.hyerim@gmail.com>
2020-08-04 18:55:43 +0900

    Refactor code

--- homework4/part1/addr.c
+++ homework4/part1/addr.c
@@ -1,43 +1,36 @@
 /**
  * Program that masks page number and offset from an
  * unsigned 32-bit address.
-**/
+ */
 
 #include <stdio.h>
 #include <unistd.h>
 
-/* page size in power of 2 */ 
-#define PAGE_SIZE_POWER2 12 
-/* The size of a page is 4 KB (12 bits)
- *
+#define PAGE_SIZE_POWEROF2 12 // Page size in power of 2
+/**
+ * The size of a page is 4 KB (12 bits)
  * A memory reference appears as:
- *
  * |------------|-----|
- *  31	    12 11     0
-*/ 
-
+ *  31        12 11  0
+ *             ^ 
+ *     PAGE_SIZE_POWEROF2
+ */ 
 
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		fprintf(stderr,"Usage: ./a.out <virtual address>\n");
-		return -1;
-	}
-	
-	int page_size;
-	int page_num;
-	int offset;
-	unsigned int reference;
+    if (argc != 2) {
+        fprintf(stderr,"Usage: ./a.out <virtual address>\n");
+        
+        return -1;
+    }
+    
+    unsigned int reference = (unsigned int) atoi(argv[1]);
+    int page_size = 0x1 << PAGE_SIZE_POWEROF2;
+    int page_num = reference / page_size;
+    int offset = = reference % page_size;
+    
+    printf("The address %d contains:\n", reference);
+    printf("page number = %d\n", page_num);
+    printf("offset = %d\n", offset);
 
-	reference = (unsigned int)atoi(argv[1]);
-	printf("The address %d contains:\n", reference);
-
-	/* FILL IN YOUR CODE HERE */
-	page_size = 0x1 << PAGE_SIZE_POWER2;
-	page_num = reference / page_size;
-	offset = reference % page_size;
-
-	printf("page number = %d\n", page_num);
-	printf("offset = %d\n", offset);
-
-	return 0;
+    return 0;
 }
